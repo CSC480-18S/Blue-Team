a25624d90dcf5f592e6a13a8661c93518682f3dd
Dor-Ron <dor.rondel@gmail.com>
2018-02-19 13:30:04 -0500

    finished source code based testing for dictionaries

--- DB/src/test/java/csc480/blue/db/DictionariesTest.java
+++ DB/src/test/java/csc480/blue/db/DictionariesTest.java
@@ -1,21 +1,26 @@
 package csc480.blue.db;
 
+import java.io.FileNotFoundException;
+import java.lang.ClassLoader;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 
 public class DictionariesTest {
 	Dictionaries dict = null;
-	String path = "./testing_assets/";
+	ClassLoader classLoader = getClass().getClassLoader();
+	String path = classLoader.getResource("testing_assets/").getPath();
 
 	@AfterEach
 	void afterEach() {
 		dict = null;
-		path = "./testing_assets/";
+		path = classLoader.getResource("testing_assets/").getPath();
 	}
 
 	@Test
-	void t00() {
+	void t00() throws FileNotFoundException {
 		path += "empty.txt";
 		dict = new Dictionaries(path, path, path);
 		assertTrue(dict.getEnglishWords().isEmpty());
@@ -24,7 +29,7 @@ public class DictionariesTest {
 	}
 
 	@Test
-	void t01() {
+	void t01() throws FileNotFoundException {
 		path += "singleton.txt";
 		dict = new Dictionaries(path, path, path);
 		assertTrue(dict.getEnglishWords().contains("SINGLEWORD"));
@@ -33,7 +38,7 @@ public class DictionariesTest {
 	}
 
 	@Test
-	void t02() {
+	void t02() throws FileNotFoundException {
 		path += "two_entries.txt";
 		dict = new Dictionaries(path, path, path);
 		assertTrue(dict.getEnglishWords().contains("WORD1"));
@@ -42,5 +47,15 @@ public class DictionariesTest {
 		assertTrue(dict.getEnglishWords().contains("WORD2"));
 		assertTrue(dict.getSpecialWords().contains("WORD2"));
 		assertTrue(dict.getBadWords().contains("WORD2"));
+	}
+
+	// Negative test
+	@Test
+	void t03() throws FileNotFoundException {
+		assertThrows(FileNotFoundException.class, () -> {
+			path += "does_not_exist.txt";
+		    dict = new Dictionaries(path, path, path);
+			System.out.println("Crashes when it's supposed to");
+		});
 	}
 }
