6fb58c7b1232cb3d2b29e3db50ed947418c4b086
rw <rwallace053@gmail.com>
2018-02-23 19:45:05 -0500

    Revert "Added documentation for setting up intellij, removed some unneeded files"
    This reverts commit 7931ba436a276b2ff3357609ee35693574a78921.

--- Engine/src/main/java/Session/Start.java
+++ Engine/src/main/java/Session/Start.java
@@ -2,8 +2,7 @@ package main.java.Session;
 
 public class Start {
     public static void main(String[] args) {
-        // to run in Windows comment out this line, (uncomment out for Linux or MacOS)
-  //      System.setProperty("java.awt.headless", "true");
+        System.setProperty("java.awt.headless", "true");
         Session session = Session.getSession();
     }
 }
