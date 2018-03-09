c7ff6e88e6563ce5a11562773a6eb0715606ee74
cvanclee <cvanclee@oswego.edu>
2018-03-09 11:00:23 -0500

    Update dummy_data.sql

--- DB/src/main/resources/dummy_data.sql
+++ DB/src/main/resources/dummy_data.sql
@@ -1,9 +1,9 @@
-INSERT INTO `CSC480Data`.`PLAYER_TABLE` (`uid`, `cumulative_score`, `longest_word`, `bonuses`, `highest_word_score`) VALUES ('kim', 200, 'magician', 6, 29);
-INSERT INTO `CSC480Data`.`PLAYER_TABLE` (`uid`, `cumulative_score`, `longest_word`, `bonuses`, `highest_word_score`) VALUES ('ken', 150, 'mavrick', 5, 30);
-INSERT INTO `CSC480Data`.`PLAYER_TABLE` (`uid`, `cumulative_score`, `longest_word`, `bonuses`, `highest_word_score`) VALUES ('yingying', 150, 'computers', 3, 20);
-INSERT INTO `CSC480Data`.`PLAYER_TABLE` (`uid`, `cumulative_score`, `longest_word`, `bonuses`, `highest_word_score`) VALUES ('christian', 92, 'light', 3, 9);
-INSERT INTO `CSC480Data`.`PLAYER_TABLE` (`uid`, `cumulative_score`, `longest_word`, `bonuses`, `highest_word_score`) VALUES ('dor', 161, 'what', 3, 12);
-INSERT INTO `CSC480Data`.`PLAYER_TABLE` (`uid`, `cumulative_score`, `longest_word`, `bonuses`, `highest_word_score`) VALUES ('john', 31, 'hat', 3, 7);
+INSERT INTO `CSC480Data`.`PLAYER_TABLE` (`uid`, `cumulative_score`, `longest_word`, `bonuses`, `highest_word_score`) VALUES ('kim', 200, 'MAGICIAN', 6, 29);
+INSERT INTO `CSC480Data`.`PLAYER_TABLE` (`uid`, `cumulative_score`, `longest_word`, `bonuses`, `highest_word_score`) VALUES ('ken', 150, 'MAVRICK', 5, 30);
+INSERT INTO `CSC480Data`.`PLAYER_TABLE` (`uid`, `cumulative_score`, `longest_word`, `bonuses`, `highest_word_score`) VALUES ('yingying', 150, 'COMPUTERS', 3, 20);
+INSERT INTO `CSC480Data`.`PLAYER_TABLE` (`uid`, `cumulative_score`, `longest_word`, `bonuses`, `highest_word_score`) VALUES ('christian', 92, 'LIGHT', 3, 9);
+INSERT INTO `CSC480Data`.`PLAYER_TABLE` (`uid`, `cumulative_score`, `longest_word`, `bonuses`, `highest_word_score`) VALUES ('dor', 161, 'WHAT', 3, 12);
+INSERT INTO `CSC480Data`.`PLAYER_TABLE` (`uid`, `cumulative_score`, `longest_word`, `bonuses`, `highest_word_score`) VALUES ('john', 31, 'HAT', 3, 7);
 
 INSERT INTO `CSC480Data`.`PLAYER_TEAM` (`uid`, `teamid`) VALUES ('kim', 'green');
 INSERT INTO `CSC480Data`.`PLAYER_TEAM` (`uid`, `teamid`) VALUES ('ken', 'gold');
@@ -27,20 +27,20 @@ insert into GAME_TABLE (game_id, gold_team_score, green_team_score) values (3, 4
 insert into TEAM_TABLE (team_name, cumulative_game_score, highest_word_score, highest_game_session_score, win_count, lose_count, tie_count, longest_word, bonuses, dirty_word) values ("green", 511, 30, 200, 3, 0, 1, "computers", 21, 5);
 insert into TEAM_TABLE (team_name, cumulative_game_score, highest_word_score, highest_game_session_score, win_count, lose_count, tie_count, longest_word, bonuses, dirty_word) values ("gold", 273, 33, 91, 0, 3, 1, "mavrick", 13, 10);
 
-insert into DIRTY_WORD_TABLE (dirty_word_id, dirty_word, attempt_usage_count) values (0, "fuck", 7);
-insert into DIRTY_WORD_TABLE (dirty_word_id, dirty_word, attempt_usage_count) values (1, "shit", 3);
-insert into DIRTY_WORD_TABLE (dirty_word_id, dirty_word, attempt_usage_count) values (2, "ass", 2);
-insert into DIRTY_WORD_TABLE (dirty_word_id, dirty_word, attempt_usage_count) values (3, "cunt", 3);
+insert into DIRTY_WORD_TABLE (dirty_word_id, dirty_word, attempt_usage_count) values (0, "FUCK", 7);
+insert into DIRTY_WORD_TABLE (dirty_word_id, dirty_word, attempt_usage_count) values (1, "SHIT", 3);
+insert into DIRTY_WORD_TABLE (dirty_word_id, dirty_word, attempt_usage_count) values (2, "ASS", 2);
+insert into DIRTY_WORD_TABLE (dirty_word_id, dirty_word, attempt_usage_count) values (3, "CUNT", 3);
 
-insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (0, "goblin", 13, 6, 0, 1);
-insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (1, "computers", 20, 9, 1, 1);
-insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (2, "magician", 29, 4, 0, 2);
-insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (3, "mavrick", 30, 3, 0, 2);
-insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (4, "love", 21, 4, 0, 1);
-insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (5, "slam", 20, 4, 0, 2);
-insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (6, "jams", 20, 4, 0, 2);
-insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (7, "ball", 15, 4, 0, 1);
-insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (8, "hat", 7, 4, 0, 0);
-insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (9, "what", 12, 4, 0, 1);
-insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (10, "light", 9, 4, 0, 0);
+insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (0, "GOBLIN", 13, 6, 0, 1);
+insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (1, "COMPUTERS", 20, 9, 1, 1);
+insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (2, "MAGICIAN", 29, 4, 0, 2);
+insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (3, "MAVRICK", 30, 3, 0, 2);
+insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (4, "LOVE", 21, 4, 0, 1);
+insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (5, "SLAM", 20, 4, 0, 2);
+insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (6, "JAMS", 20, 4, 0, 2);
+insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (7, "BALL", 15, 4, 0, 1);
+insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (8, "HAT", 7, 4, 0, 0);
+insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (9, "WHAT", 12, 4, 0, 1);
+insert into VALID_WORD_TABLE (word_id, word, value, length, is_extension, bonuses_used) values (10, "LIGHT", 9, 4, 0, 0);
 
