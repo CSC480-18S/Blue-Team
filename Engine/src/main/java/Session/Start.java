d3328b1dffc3b0e0e11ae89388f3dda77b550ce9
rw <rwallace053@gmail.com>
2018-03-25 21:45:29 -0400

    New kiosk GUI, Updated webpage tiles, Updated first move checker

--- Engine/src/main/java/Session/Start.java
+++ Engine/src/main/java/Session/Start.java
@@ -4,7 +4,6 @@ public class Start {
     public static void main(String[] args) {
         // Comment out below line to run in Windows
         //System.setProperty("java.awt.headless", "true");
-        System.out.println("*********************************" + java.awt.GraphicsEnvironment.isHeadless());
         Session.getSession();
     }
 }
