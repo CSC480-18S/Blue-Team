7249944fad744d58f53f46ab018f9a07bb6b9494
rw <rwallace053@gmail.com>
2018-04-06 14:50:42 -0400

    Revert "Revert "Made score available from server""
    This reverts commit 727c1c65d634e056555f614c63e7b24db3768f89.

--- Engine/src/main/java/Servlet/Servlet.java
+++ Engine/src/main/java/Servlet/Servlet.java
@@ -118,6 +118,10 @@ public class Servlet extends HttpServlet {
             } else if (req.equals("getboard")) {
                 out.write(Session.getSession().getBoardJSON());
 
+            } else if (req.equals("getscore")) {
+                String mac = request.getParameter("macAddress");
+                out.write(EventHandler.scoreHandler(mac));
+
             }else {
                 // Unknown request
                 out.write(EventHandler.unknownHandler());
