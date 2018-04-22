0d0ddae21543f6a55767c98a75170745b185e899
gcampbe3 <Gcampbe3@oswego.edu>
2018-04-22 16:47:08 -0400

    Fix for blank tiles

--- Engine/src/main/java/Session/Session.java
+++ Engine/src/main/java/Session/Session.java
@@ -244,15 +244,18 @@ public class Session {
     // Validate word and place on board
     public String playWord(int startX, int startY, boolean horizontal, String word, User user) {
         String initialLetters = word;
+        System.out.println(initialLetters + " ******************************** initial letter input");
         TileGenerator tg = TileGenerator.getInstance();
         // Check if word length is less than 11,
         // it will cause errors if too big.
         // This should never happen but just in case..
-        if (word.length() > 11) {
+        String tempWord = word.replace("_","");
+        System.out.println("Word minus _ " + tempWord);
+        if (tempWord.length() > 11) {
             return "Please play a shorter word?...";
         }
 
-        if (word.length() == 1) {
+        if (tempWord.length() == 1) {
             if ((startX + 1 < BOARD_WIDTH && board.getBoard()[startX + 1][startY].getTile() != null) || (startX > 0  && board.getBoard()[startX - 1][startY].getTile() != null)) {
                 horizontal = true;
             }
@@ -282,6 +285,10 @@ public class Session {
                 wordTileBuilder.add(tg.getTile(each));
         }
 
+        for(Tile each : wordTileBuilder){
+            System.out.println(each.toString());
+        }
+
 
         Tile[] wordTiles = new Tile[wordTileBuilder.size()];
         for(int i = 0; i < wordTileBuilder.size(); i++){
@@ -290,6 +297,7 @@ public class Session {
 
         Object[] result = validator.isValidPlay(new Move(startX, startY, horizontal, wordTiles, user));
 
+        word = ((Move) result[1]).getWordString();
         if ((int) result[0] == 1) {
             board.placeWord(startX, startY, horizontal, word);
             int score = calculateMovePoints((Move)result[1]);
