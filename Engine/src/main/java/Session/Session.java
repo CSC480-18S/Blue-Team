02f482df020216a164d3eee99f08778f843f70b9
Bohdan Yevdokymov <bohdan91@gmail.com>
2018-04-26 15:25:14 -0400

    forgot about gold team

--- Engine/src/main/java/Session/Session.java
+++ Engine/src/main/java/Session/Session.java
@@ -237,7 +237,9 @@ public class Session {
                     newPlayer.setHand(hand);
                 }
                 users[i] = newPlayer;
-                setPlayerTimer();
+                if(currentTurn == i) {
+                    setPlayerTimer();
+                }
                 gui.updateUsers(users);
                 gui.printGameLog(newPlayer.getUsername() + " has joined " + team + " Team");
                 return "JOINED";
